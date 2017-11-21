664cec5c9c18fe4bd097ae292afc3478e268ef62
Aaron Silverstein <adsilvercomp@Aarons-MBP.fios-router.home>
2017-11-21 11:56:28 -0500

    update

--- app/routing/api.js
+++ app/routing/api.js
@@ -7,8 +7,6 @@ var friendData = require("../data/friends.js");
 
 
 
-
-
 //why is app being passed into this function?
 module.exports = function (app) {
     app.post("/api/friends", function (req, res) {
@@ -40,48 +38,29 @@ module.exports = function (app) {
                 console.log(dif);
                 difArray.push(dif);
             }
-        // //*****if dif is less than variable match.diff replace the match object data with the friend who has lowest absolute difference
-            // if (dif < match.diff) {
-            //     match.name = friendData[i].name;
-            //     match.photo = friendData[i].photo;
-            //     match.diff = dif;
-            // }
+       
             var sum = difArray.reduce((a, b) => a + b);
             allDifs.push(sum);
-            console.log("allDifs array");
-            console.log(allDifs);
-            //sort the allDifs array and select the number at position 0
-            var sortedDifs = allDifs.sort(function(a, b){return a-b});
-            //Declare it as variable called smallestDif
-            var smallestDif = sortedDifs[0];
-            console.log("smallestDif");
-            console.log(smallestDif);
-            //loop through the allDifs array. 
-            for (var x=0; x<allDifs.length; x++){
-                //if smallestDif === alldifs[i]
-                if(smallestDif === allDifs[x]){
-                    console.log("This is the one");
-                    console.log(allDifs[x]);
-                    console.log(friendData[x]);
-                    
-                    // closestFriend.push(friendData[x]);
-                    
-                }
             
-            }
-            
-            
-            //push the closest friend[i] to a new array
-            //
-
-            //have an array of arrays go through that array and find the lowest number
-            //push all of those difference arrays to a single array
-            //each of their indexes in the global array is still going to refer to the index of the friend
-            //loop over that array of arrays and sum their values. 
-            //each time you loop over it you can have a maximum variable and a max index
-            //if the sum of this current array is greater than the max, set the max equal to this sum. 
         
         }
+        //unsorted version of array for the purposes of matching the smallest difference with it's position in the friendData array.
+        var unsortedDifs = allDifs.slice(0,4);
+        console.log("unsorted");
+        console.log(unsortedDifs);
+        //sort the allDifs array and select the number at position 0
+        var sortedDifs = allDifs.sort(function(a, b){return a-b});
+        //Declare it as variable called smallestDif
+        var smallestDif = sortedDifs[0];
+        console.log("smallestDif");
+        console.log(smallestDif);
+        
+        console.log("allDifs array");
+        console.log(allDifs);
+              var number =  unsortedDifs.indexOf(smallestDif);
+              console.log("index of the smallest difference");
+              console.log(number);
+              console.log(friendData[number]);
         //*****this pushes the userData to the friendArray
         // friendData.push(userData);
         // res.json(match);
